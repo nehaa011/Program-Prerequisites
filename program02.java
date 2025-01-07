fa97d204a8c327a2f55201740442cabb9bb30df0
nehaa011 <nehagour46420@gmail.com>
2025-01-08 00:59:31 +0530

    problem 2

---
+++ program02.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class program02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("The Sum is:" + sum);
+
+    }
+}
